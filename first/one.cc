5328143fcdab01755d96241bdbbd4e1e735d3d2c
Konstantin Stepanov <k.stepanov98@yandex.ru>
2020-04-02 14:52:47 +0300

    optimize simulate function

--- first/one.cc
+++ first/one.cc
@@ -7,15 +7,15 @@ using namespace Eigen;
 
 namespace plt = matplotlibcpp;
 
-double simulate(int time, int iterations, int state, Matrix2d &transitionMatrix) {
+std::pair<double, double> simulate(int time, int iterations, Matrix2d &transitionMatrix) {
   std::mt19937 rng;
   rng.seed(std::random_device()());
   std::uniform_real_distribution<float> dist(0.f, 1.f);
 
-  int state_count = 0;
-  
-  if (state == 1 && time == 0) return 0.0;
-  int currentState = state;
+  int state0_count = 0;
+  int state1_count = 0;
+  int currentState = 0;
+
   for (int i = 0; i < iterations; i++) {
     for (int j = 0; j < time; j++) {
       if (currentState == 0) {
@@ -24,18 +24,19 @@ double simulate(int time, int iterations, int state, Matrix2d &transitionMatrix)
         if (dist(rng) > transitionMatrix(1, 1)) currentState = 0;
       }
     }
-    if (currentState == state) state_count++;
+    if (currentState == 0) state0_count++;
+    if (currentState == 1) state1_count++;
   }
-  return double(state_count) / iterations;
+  return {double(state0_count) / iterations, double(state1_count) / iterations};
 }
 
-std::vector<double> theoretic_probabilities(int time, int state, Matrix2d &transitionMatrix) {
-  std::vector<double> th_prob;
-  state == 0 ? th_prob.push_back(1.0) : th_prob.push_back(0.0);
+std::vector<std::pair<double, double>> theoretic_probabilities(int time, Matrix2d &transitionMatrix) {
+  std::vector<std::pair<double, double>> th_prob;
+  th_prob.push_back({1.0, 0.0});
   RowVector2d p0(1.0, 0.0);
   for (int t = 1; t < time; t++) {
     p0 = p0 * transitionMatrix;
-    th_prob.push_back(p0(state));
+    th_prob.push_back({p0(0), p0(1)});
   }
   return th_prob;
 }
@@ -51,12 +52,16 @@ int main() {
   std::vector<double> pr1(time);
   std::vector<double> x(time);
 
-  std::vector<double> th_pr0 = theoretic_probabilities(time, 0, transitionMatrix);
-  std::vector<double> th_pr1 = theoretic_probabilities(time, 1, transitionMatrix);
-
+  std::vector<std::pair<double, double>> th_pr = theoretic_probabilities(time, transitionMatrix);
+  std::vector<double> th_pr0(time);
+  std::vector<double> th_pr1(time);
+  
   for (int t = 0; t < time; t++) {
-    pr0[t] = simulate(t, iterations, 0, transitionMatrix);
-    pr1[t] = simulate(t, iterations, 1, transitionMatrix);
+    std::pair<double, double> prob = simulate(t, iterations, transitionMatrix);
+    pr0[t] = prob.first;
+    pr1[t] = prob.second;
+    th_pr0[t] = th_pr[t].first;
+    th_pr1[t] = th_pr[t].second;
     x[t] = t;
   }
 
